c3230ffc893f25cf0b59544f5fa5955fa840f95e
zeecm <114894488+zeecm@users.noreply.github.com>
2023-08-02 12:08:50 +0000

    Fix actions

--- tests/test_template.py
+++ tests/test_template.py
@@ -1,5 +1,15 @@
+import pytest
+
 from python_package_template.template import template_add
 
 
-def test_template_add():
-  assert template_add(1, 2) == 3
+@pytest.mark.parametrize(
+    "num1, num2, result",
+    [
+        (1, 2, 3),
+        (2, 2, 4),
+        (5, 7, 12),
+    ],
+)
+def test_template_add(num1, num2, result):
+    assert template_add(num1, num2) == result
